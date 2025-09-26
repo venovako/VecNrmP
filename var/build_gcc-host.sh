63c42ae40bda85a03991da72132babd1213d7b54
Vedran Novakovic <venovako@venovako.eu>
2025-09-26 19:19:37 +0200

    further updates

---
+++ var/build_gcc-host.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# !!! has to be run from this directory !!!
+if [ -z "${GNU}" ]
+then
+	echo "GNU=suffix ./build_gcc-host.sh"
+else
+	cd ../../libpvn/src
+	make COMPILER=gcc COMPILER_SUFFIX=${GNU} NDEBUG=3 SAFE=ev2,jv2,sv2,nrm GMP=$HOME/gnu MPFR=$HOME/gnu LAPACK=$HOME/lapack clean all
+	cd ../../VecNrmP/src
+	make GNU=${GNU} clean all
+	cd ../var
+fi
