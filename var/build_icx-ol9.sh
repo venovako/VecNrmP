2e6382208b88e30c63b85702a6b50f72625f01cf
Vedran Novakovic <venovako@venovako.eu>
2025-09-26 18:23:30 +0200

    further updates

--- var/build_icx-ol9.sh
+++ var/build_icx-ol9.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
-# !!! HAS TO BE RUN FROM THIS DIRECTORY !!!
-if [ "${MARCH}" != "Host" ]
+# !!! has to be run from this directory on Oracle Linux 9 with the static glibc added manually !!!
+if [ -z "${TGT}" ]
 then
-	cd ../../libpvn/src
-	scl enable gcc-toolset-14 "make COMPILER=icx NDEBUG=3 SAFE=ev2,jv2,sv2,nrm GMP=/opt/gmp MPFR=/opt/mpfr LAPACK=../../lapack MARCH=${MARCH} STATIC=-s clean all"
-	mkdir -pv ../../dist
-	mv -fv libpvn.* pvn*.exe ../etc/gen_cbar.exe ../../dist
-	scl enable gcc-toolset-14 "make COMPILER=icx NDEBUG=3 SAFE=ev2,jv2,sv2,nrm GMP=/opt/gmp MPFR=/opt/mpfr LAPACK=../../lapack STATIC=-s clean all"
-	cp -fv ../../dist/libpvn.* .
-	cd ../../VecNrmP/src
-	if [ -z "${GNU}" ]
-	then
-		scl enable gcc-toolset-14 "make MARCH=${MARCH} clean all"
-	else
-		scl enable gcc-toolset-14 "make MARCH=${MARCH} GNU=${GNU} clean all"
-	fi
-	cp -fv *.exe ../../dist
-	cd ../..
-	rm -fv dist.tar.xz
-	tar cvf dist.tar dist
-	xz -zv9e dist.tar
-	cd VecNrmP/var
+	echo "TGT=target_architecture [GNU=gfortran_suffix] ./build_icx-ol9.sh"
 else
-	echo "Please build the usual way."
+	if [ "${TGT}" != "Host" ]
+	then
+		cd ../../libpvn/src
+		scl enable gcc-toolset-14 "make COMPILER=icx NDEBUG=3 SAFE=ev2,jv2,sv2,nrm GMP=/opt/gmp MPFR=/opt/mpfr LAPACK=../../lapack MARCH=${TGT} STATIC=-s clean all"
+		mkdir -pv ../../dist
+		mv -fv libpvn.* pvn*.exe ../etc/gen_cbar.exe ../../dist
+		scl enable gcc-toolset-14 "make COMPILER=icx NDEBUG=3 SAFE=ev2,jv2,sv2,nrm GMP=/opt/gmp MPFR=/opt/mpfr LAPACK=../../lapack STATIC=-s clean all"
+		cp -fv ../../dist/libpvn.* .
+		cd ../../VecNrmP/src
+		if [ -z "${GNU}" ]
+		then
+			scl enable gcc-toolset-14 "make MARCH=${TGT} clean all"
+		else
+			scl enable gcc-toolset-14 "make MARCH=${TGT} GNU=${GNU} clean all"
+		fi
+		cp -fv *.exe ../../dist
+		cd ../..
+		rm -fv dist.tar.xz
+		tar cvf dist.tar dist
+		xz -zv9e dist.tar
+		cd VecNrmP/var
+	else
+		echo "Please build the usual way."
+	fi
 fi
