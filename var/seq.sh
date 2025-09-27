03611241dc5be4e94cd9ec406229229c3d914b73
Vedran Novakovic <venovako@venovako.eu>
2025-09-27 11:50:35 +0200

    further updates

--- var/seq.sh
+++ var/seq.sh
@@ -3,9 +3,9 @@ source ./env.sh
 cd ../../libpvn/src
 for O in nd ns ud us
 do
-	echo -n "Running $O.sh ... "
-	../../VecNrmP/var/$O.sh
+	echo -n "Running ${O}.sh ... "
+	../../VecNrmP/var/${O}.sh
 	echo "done"
-	mv -fv $O.out $O.err ../../VecNrmP/var
+	mv -fv ${O}*.out ${O}*.err ../../VecNrmP/var
 done
 unset O
--- var/seqp.sh
+++ var/seqp.sh
@@ -3,9 +3,9 @@ source ./env.sh
 cd ../src
 for O in ndp udp
 do
-	echo -n "Running $O.sh ... "
-	../var/$O.sh
+	echo -n "Running ${O}.sh ... "
+	../var/${O}.sh
 	echo "done"
-	mv -fv $O.out $O.err ../var
+	mv -fv ${O}*.out ${O}*.err ../var
 done
 unset O
