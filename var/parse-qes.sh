5541878d2cd5597523e6ddc3c697721b2e8a7062
Vedran Novakovic <venovako@venovako.eu>
2025-12-11 01:04:35 +0100

    further updates

--- var/parse-qes.sh
+++ var/parse-qes.sh
@@ -2,13 +2,13 @@
 for O in dn du sn su
 do
 	echo -n "Creating $O.csv ... "
-	echo '"RUN", "SEED", "MPV", "MPE", "MPT", "LAV", "LAE", "LAT", "M2V", "M2E", "M2T", "RFV", "RFE", "RFT", "RHV", "RHE", "RHT", "RXV", "RXE", "RXT", "RYV", "RYE", "RYT", "RZV", "RZE", "RZT", "CRV", "CRE", "CRT"' > $O.csv
+	echo '"RUN", "SEED", "MPV", "MPE", "MPT", "LAV", "LAE", "LAT", "MKV", "MKE", "MKT", "RBV", "RBE", "RBT", "M2V", "M2E", "M2T", "RFV", "RFE", "RFT", "RHV", "RHE", "RHT", "RXV", "RXE", "RXT", "RYV", "RYE", "RYT", "RZV", "RZE", "RZT", "CRV", "CRE", "CRT"' > $O.csv
 	for ((I=1;I<=31;++I))
 	do
 		printf "%2d, " $I >> $O.csv
 #		S=`grep SEED $O$I.err | cut -d'=' -f2 | tr -d ' '`
 #		echo -n "$S" >> $O.csv
-		for F in mp la m2 rf rh rx ry rz cr
+		for F in mp la mk rb m2 rf rh rx ry rz cr
 		do
 			L=`grep $F $O$I.out | tr -s ' '`
 			V=`echo "$L" | cut -d' ' -f2`
