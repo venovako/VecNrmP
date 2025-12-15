a330184bc450709a2fed35d81424803aabab385d
Vedran Novakovic <venovako@venovako.eu>
2025-12-15 17:11:34 +0100

    further updates

--- var/pcat.sh
+++ var/pcat.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# Usage: ./pcat.sh NRUN PREFIX
-rm -fv $2.out
+# Usage: ./pcat.sh RUNs Ps PREFIX
+rm -fv $3.out
 for ((I=1;I<=$1;++I))
 do
-	echo "RUN $I" >> $2.out
-	cat $2$1.out >> $2.out
+	echo "RUN $I" >> $3.out
+	cat $3$1.out >> $3.out
 done
+../src/parse_p.exe $1 $2 $3
 unset I
