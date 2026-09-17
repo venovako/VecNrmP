1e53ebde45e1a2ba1da77d60c208e8f00e9652f9
Vedran Novakovic <venovako@venovako.eu>
2026-09-17 21:36:47 +0200

    further updates

---
+++ var/srun.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+./nrm2.exe  1073741824  -84225929407270 >  su1.out 2>  su1.err &
+./nrm2.exe -1073741824  -25944849203584 >  sn1.out 2>  sn1.err &
+wait
+./nrm2.exe  1073741824 -133283450964053 >  su2.out 2>  su2.err &
+./nrm2.exe -1073741824 -153077598009512 >  sn2.out 2>  sn2.err &
+wait
+./nrm2.exe  1073741824  -17688476434090 >  su3.out 2>  su3.err &
+./nrm2.exe -1073741824  -39162661063339 >  sn3.out 2>  sn3.err &
+wait
+./nrm2.exe  1073741824  -98101099171013 >  su4.out 2>  su4.err &
+./nrm2.exe -1073741824 -193905104641114 >  sn4.out 2>  sn4.err &
+wait
+./nrm2.exe  1073741824  -43262801938392 >  su5.out 2>  su5.err &
+./nrm2.exe -1073741824 -125289815541128 >  sn5.out 2>  sn5.err &
+wait
+./nrm2.exe  1073741824 -143838884838290 >  su6.out 2>  su6.err &
+./nrm2.exe -1073741824 -271689591534628 >  sn6.out 2>  sn6.err &
+wait
+./nrm2.exe  1073741824 -214786254272449 >  su7.out 2>  su7.err &
+./nrm2.exe -1073741824 -149243941829742 >  sn7.out 2>  sn7.err &
+wait
+./nrm2.exe  1073741824 -132582279127473 >  su8.out 2>  su8.err &
+./nrm2.exe -1073741824 -153477789009873 >  sn8.out 2>  sn8.err &
+wait
+./nrm2.exe  1073741824 -143967321166012 >  su9.out 2>  su9.err &
+./nrm2.exe -1073741824 -102464862945811 >  sn9.out 2>  sn9.err &
+wait
+./nrm2.exe  1073741824  -53715765044733 > su10.out 2> su10.err &
+./nrm2.exe -1073741824 -264156136387528 > sn10.out 2> sn10.err &
+wait
+./nrm2.exe  1073741824 -189489026904977 > su11.out 2> su11.err &
+./nrm2.exe -1073741824  -48209008984770 > sn11.out 2> sn11.err &
+wait
+./nrm2.exe  1073741824 -261677460603461 > su12.out 2> su12.err &
+./nrm2.exe -1073741824    -926644888384 > sn12.out 2> sn12.err &
+wait
+./nrm2.exe  1073741824 -196194720220788 > su13.out 2> su13.err &
+./nrm2.exe -1073741824 -269742808093568 > sn13.out 2> sn13.err &
+wait
+./nrm2.exe  1073741824  -11625128473918 > su14.out 2> su14.err &
+./nrm2.exe -1073741824 -133140834589919 > sn14.out 2> sn14.err &
+wait
+./nrm2.exe  1073741824 -102881259758035 > su15.out 2> su15.err &
+./nrm2.exe -1073741824    -948814433127 > sn15.out 2> sn15.err &
+wait
+./nrm2.exe  1073741824  -22363808349355 > su16.out 2> su16.err &
+./nrm2.exe -1073741824 -257418450071907 > sn16.out 2> sn16.err &
+wait
+./nrm2.exe  1073741824 -107432400246448 > su17.out 2> su17.err &
+./nrm2.exe -1073741824 -210415706860389 > sn17.out 2> sn17.err &
+wait
+./nrm2.exe  1073741824 -223413620295979 > su18.out 2> su18.err &
+./nrm2.exe -1073741824 -188935437690893 > sn18.out 2> sn18.err &
+wait
+./nrm2.exe  1073741824  -38718330254922 > su19.out 2> su19.err &
+./nrm2.exe -1073741824 -217646148643322 > sn19.out 2> sn19.err &
+wait
+./nrm2.exe  1073741824  -69349230660948 > su20.out 2> su20.err &
+./nrm2.exe -1073741824 -199941492037421 > sn20.out 2> sn20.err &
+wait
+./nrm2.exe  1073741824  -74576340801069 > su21.out 2> su21.err &
+./nrm2.exe -1073741824 -225480702134323 > sn21.out 2> sn21.err &
+wait
+./nrm2.exe  1073741824 -131770223446223 > su22.out 2> su22.err &
+./nrm2.exe -1073741824  -70194609683243 > sn22.out 2> sn22.err &
+wait
+./nrm2.exe  1073741824 -148678105992986 > su23.out 2> su23.err &
+./nrm2.exe -1073741824 -260015387711235 > sn23.out 2> sn23.err &
+wait
+./nrm2.exe  1073741824 -176742582567176 > su24.out 2> su24.err &
+./nrm2.exe -1073741824 -232646152833469 > sn24.out 2> sn24.err &
+wait
+./nrm2.exe  1073741824 -114328562737265 > su25.out 2> su25.err &
+./nrm2.exe -1073741824 -217361901530733 > sn25.out 2> sn25.err &
+wait
+./nrm2.exe  1073741824 -174652653472508 > su26.out 2> su26.err &
+./nrm2.exe -1073741824 -164044943949429 > sn26.out 2> sn26.err &
+wait
+./nrm2.exe  1073741824 -209031429304191 > su27.out 2> su27.err &
+./nrm2.exe -1073741824 -189221027838123 > sn27.out 2> sn27.err &
+wait
+./nrm2.exe  1073741824  -77925623070312 > su28.out 2> su28.err &
+./nrm2.exe -1073741824 -127895705221835 > sn28.out 2> sn28.err &
+wait
+./nrm2.exe  1073741824  -78828782895247 > su29.out 2> su29.err &
+./nrm2.exe -1073741824  -46173357579968 > sn29.out 2> sn29.err &
+wait
+./nrm2.exe  1073741824 -178392018673735 > su30.out 2> su30.err &
+./nrm2.exe -1073741824 -147595130650356 > sn30.out 2> sn30.err &
+wait
+./nrm2.exe  1073741824 -228502501109146 > su31.out 2> su31.err &
+./nrm2.exe -1073741824  -27472802710492 > sn31.out 2> sn31.err &
+wait
+sudo poweroff
